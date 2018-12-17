b6bae69326f9b537a405016683da124cfda8cdf5
Cam <cam@camshepherd.com>
2018-12-17 13:42:27 +0000

    Added getting of points centred on target location

--- website/Manager.py
+++ website/Manager.py
@@ -3,10 +3,15 @@ import socketserver
 from urllib import parse
 import json
 from os import curdir, sep
-
+from scipy.spatial import *
+import pandas as pd
 
 class MyServer(server.BaseHTTPRequestHandler):
 
+    def __init__(self):
+        self.weather = pd.read_csv('met_data_complete.csv', header=[0, 1], index_col=[0, 1])
+        self.weather_locations = self.weather.columns
+        self.the_tree = cKDTree([thing for thing in self.weather_locations])
     def serve_test_page(self):
         f = open(curdir + sep + '/test_page.html')
         self.send_response(200)
@@ -29,7 +34,10 @@ class MyServer(server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(params).encode('utf-8'))
             self.server.path = self.path
 
+    def get_locations(self,easting,northing,radius):
+        results = self.the_tree.query_ball_point([(easting, northing)], r=radius)
+        return results
 
-
+    
 httpd = socketserver.TCPServer(("", 8080), MyServer)
 httpd.serve_forever()
